f663795be83a678776bed4507242886441777240
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-07-08 18:57:18 +0700

    remove unnecesarry code by opening trees and storing it firsthand

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,55 +1,69 @@
 use std::{error::Error, path::Path, str::from_utf8};
-use sled::{Config, Db};
+use sled::{Config, Db, Tree};
 
 use crate::metadata::Metadata;
 
 #[derive(Debug)]
 struct DB {
-    db: Db
+    data_tree: Tree,
+    meta_tree: Tree
 }
 
 impl DB {
-    pub fn new(path: &Path) -> Result<Db, sled::Error> {
+    pub fn new(path: &Path) -> Result<DB, sled::Error> {
         let db = Config::new()
             .path(path)
             .cache_capacity(512 * 1024 * 1024) 
             .open()?;
-        let _ = db.open_tree("data_tree")?;
-        let _ = db.open_tree("freq_tree")?;
-        Ok(db)
-    }
-
-    pub fn set(&self, key: &str, val: &str) -> Result<(), sled::Error>{
-        let db = &self.db;
         let data_tree = db.open_tree("data_tree")?;
-        let freq_tree = db.open_tree("freq_tree")?;
+        let meta_tree = db.open_tree("freq_tree")?;
+        Ok(
+            DB { data_tree, meta_tree }
+        )
+    }
+    pub fn set(&self, key: &str, val: &str) -> Result<(), sled::Error>{
+        let data_tree = &self.data_tree;
+        let freq_tree = &self.meta_tree;
         let _ =data_tree.insert(key.as_bytes(), val.as_bytes())?;
         let _ = freq_tree.insert(key.as_bytes(), Metadata::new().to_u8().expect("Cant serialize to u8"))?;
         
         todo!()
     }
 
-    pub fn get(&self, key: &str) -> Result<String, Box<dyn Error>> {
-        let db = &self.db;
-        let byte = &key.as_bytes();
-        let data_tree = db.open_tree("data_tree")?;
-        let freq_tree = db.open_tree("freq_tree")?;
-        // FIX: Proper error handling to take in Result<Option<>> rather than just Result<>
+    pub fn set_overwrite_metadata(&self, key: &str, val: &str) -> Result<(), sled::Error>{
+        let data_tree = &self.data_tree;
+        let freq_tree = &self.meta_tree;
+        let _ =data_tree.insert(key.as_bytes(), val.as_bytes())?;
+        let _ = freq_tree.insert(key.as_bytes(), Metadata::new().to_u8().expect("Cant serialize to u8"))?;
         
-        let val = data_tree.get(byte)?.expect("val is not found");
+        Ok(())
+    }
+
+    pub fn get(&self, key: &str) -> Result<String, Box<dyn Error>> {
+        let data_tree = &self.data_tree;
+        let byte = &key.as_bytes();
+        let val = data_tree.get(byte)?.expect("Key returns none");
+
+        Ok(from_utf8(&val.to_vec())?.to_string())
+    }
+
+    pub fn increment_frequency(&self, key: &str) -> Result<(), Box<dyn Error>> {
+        let freq_tree = &self.meta_tree;
+        let byte = &key.as_bytes();
 
         let metadata = freq_tree.get(byte)?.expect("freq is not found");
         let mut meta = Metadata::from_u8(&metadata.to_vec()[..]).expect("Cant deserialize from freq_tree to Metadata");
         meta.freq_incretement();
-        let _ = freq_tree.insert(&val, meta.to_u8()?)?;
-        Ok(from_utf8(&val.to_vec())?.to_string())
+        let _ = freq_tree.insert(&key, meta.to_u8()?)?;
+
+        Ok(())
+
     }
 
     pub fn remove(&self, key: &str) -> Result<(), Box<dyn Error>> {
-        let db = &self.db;
+        let data_tree = &self.data_tree;
+        let freq_tree = &self.meta_tree;
         let byte = &key.as_bytes();
-        let data_tree = db.open_tree("data_tree")?;
-        let freq_tree = db.open_tree("freq_tree")?;
         let _ = data_tree.remove(byte)?;
         let _ =freq_tree.remove(byte)?;
         Ok(())
