e4c55e80c7879ae24231fd83c1e87551d75a4235
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-07-08 21:36:19 +0700

    My own error type

---
+++ src/db/errors.rs
@@ -0,0 +1,17 @@
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug)]
+enum TransientError {
+    IncretmentFailure
+}
+
+impl Display for TransientError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TransientError::IncretmentFailure => writeln!(f, "Incretment has failed")
+        }
+    }
+}
+
+
+impl Error for TransientError {}
