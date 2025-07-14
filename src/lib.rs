908993fc52322ead9f09ddb9254dabb2ce2a4f5d
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-07-14 20:40:15 +0700

    DB struct documentation

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,24 @@ use std::{
 pub mod db;
 pub mod metadata;
 
+/// This is the main struct which represents the database.
+///
+/// It stores 2 sled::Tree, instead of a single sled::Db,
+/// Because in almost all of the functions trees needs to be open which wastes some perfomance
+/// passing trees from the struct deletes the need of constant opening of the trees which increase
+/// performance
 #[derive(Debug)]
 pub struct DB {
+    /// the tree which stores the key and value
     data_tree: Arc<Tree>,
+    /// the tree which stores the key and the metadata
     meta_tree: Arc<Tree>,
+    /// the tree which stores the ttl timestamp and the key
     ttl_tree: Arc<Tree>,
+    /// the thread that is used to iterate checks on the key with the earliest ttl timestamp
     ttl_thread: Option<JoinHandle<Result<(), TransientError>>>,
+    /// the shutdown bool which tells the tt_thread that the DB is dropped so that the ttl_thread
+    /// can join succesfully
     shutdown: Arc<AtomicBool>,
 }
 
